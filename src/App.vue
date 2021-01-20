3ee8dcfbb70bc1a5251bfff185e75ed8d0753a2f
Higor Castilho <castilhohf@gmail.com>
2021-01-20 04:17:59 -0300

    feat: use EnterpriseFeed view

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <EnterpriseFeed />
+  </div>
+</template>
+
+<script>
+import EnterpriseFeed from './views/EnterpriseFeed.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EnterpriseFeed
+  }
+}
+</script>
+
