8efeefc856aef03beedc7bf540c987b505e23524
Higor Castilho <castilhohf@gmail.com>
2021-01-20 04:23:54 -0300

    implement EnterpriseCard component

---
+++ src/components/EnterpriseCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card-wrapper">
+    <h1>{{enterprise.id}}</h1>
+    <img :src="enterprise.image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EnterpriseCard',
+  props: {
+    enterprise: {
+			type: Object,
+			required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	h1 {
+		color: green;
+	}
+	h2 {
+		color: black;
+	}
+
+	.card-wrapper {
+		background: pink;
+	}
+
+</style>
